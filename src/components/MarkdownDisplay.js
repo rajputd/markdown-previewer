9d72b6ea073283c8701d67f10a3687bb1866ad14
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-11 10:57:35 -0400

    Add MarkdownDisplay component

---
+++ src/components/MarkdownDisplay.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class MarkDownDisplay extends Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <textarea value={this.props.markdown}/>
+    )
+  }
+}
